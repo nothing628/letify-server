39388e2fa0aaa89429ff850ae2135100c01249aa
nothing628 <yogyphang@gmail.com>
2021-12-11 19:11:28 +0700

    create wallet controller

---
+++ app/Controllers/Http/WalletsController.ts
@@ -0,0 +1,40 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class WalletsController {
+  async index(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+
+  async create(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+  async store(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+  async show(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+  async edit(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+  async update(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+  async destroy(ctx: HttpContextContract) {
+    return {
+      success: true,
+    }
+  }
+}
