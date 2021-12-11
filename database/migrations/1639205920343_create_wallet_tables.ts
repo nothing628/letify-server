a1689d503463be2870908f88f96fd1efc9fcf891
nothing628 <yogyphang@gmail.com>
2021-12-11 14:40:37 +0700

    create wallet table

---
+++ database/migrations/1639205920343_create_wallet_tables.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class CreateWalletTables extends BaseSchema {
+  protected tableName = 'wallets'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').notNullable()
+      table.string('label', 50).notNullable();
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+
+    this.schema.raw(`alter table ${this.tableName} alter id set default gen_random_uuid()`)
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
