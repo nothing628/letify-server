50dcc0145f308f13c5f56e51c7b4b30899a7e68f
nothing628 <yogyphang@gmail.com>
2021-12-11 13:51:43 +0700

    add supertest for testing http request

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,24 @@
 import test from 'japa'
+import { JSDOM } from 'jsdom'
+import supertest from 'supertest'
 
-test.group('Example', () => {
-  test('assert sum', (assert) => {
-    assert.equal(2 + 2, 4)
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Welcome', () => {
+  test('ensure home page works', async (assert) => {
+    /**
+     * Make request
+     */
+    const response = await supertest(BASE_URL).get('/').expect(200)
+    const { body } = response
+
+    /**
+     * Construct JSDOM instance using the response HTML
+     */
+    // const { document } = new JSDOM(text).window
+
+    // const title = document.querySelector('.title')
+    // assert.exists(title)
+    assert.deepEqual(body, {hello: "world"});
   })
 })
